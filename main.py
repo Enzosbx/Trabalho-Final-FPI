b823269d872cc849fc8d6ca7b72505d211d16492
Enzo Sergi B. Xavier <91389519+Enzosbx@users.noreply.github.com>
2023-03-12 14:13:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ import utils
 init = image.Image()
 source_img = init.get_source_img()
 target_img = init.get_target_img()
-utils.energy_boundary_minimization(init.get_target_img(), init.get_source_img(), init.get_grab_and_cut_mask())
+mask = init.get_grab_and_cut_mask()
+
+
+cur_boundary = user.get_user_boundary()
+cut_line_coordinates = utils.define_cut_line_coordinates(mask, cur_boundary)
+print(cut_line_coordinates)
+
+#utils.energy_boundary_minimization(init.get_target_img(), init.get_source_img(), init.get_grab_and_cut_mask())
+
+
 # cv.imshow('Source image', )
 # cv.imshow('Target image', )
 # cv.waitKey(0)
 # cv.destroyAllWindows()
-
